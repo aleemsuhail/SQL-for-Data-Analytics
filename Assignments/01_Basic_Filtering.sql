271b65ccd48edf6f3b08a61a345582acae0cd79a
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 17:59:39 +0400

    Add solution for Q13 (Filtering by Manager ID)

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -225,3 +225,19 @@ insert into employee values(5,'E',null);
 insert into employee values(6,'F',null);
 
 Select * from employee Where empid IN (1, 2, 8);
+
+
+
+
+
+-- Q13.Write a SQL query to fetch the employee details who are working under mgrid= 5;
+
+Create table employee(empid number,empname varchar2(10),Mgrid number);
+insert into employee values(1,'A',4);
+insert into employee values(2,'B',5);
+insert into employee values(3,'C',6);
+insert into employee values(4,'D',5);
+insert into employee values(5,'E',5);
+insert into employee values(6,'F',6);
+
+Select * from employee Where mgrid = 5;
