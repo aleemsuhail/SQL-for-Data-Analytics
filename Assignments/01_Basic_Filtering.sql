0a8b3cf2ade4b15070ac57866a4dbc02c60b13db
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 17:30:47 +0400

    Add solution for Q9 (Range filtering with IN operator)

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -151,3 +151,18 @@ insert into student values(4,'D','Phy','75');
 insert into student values(4,'D','Che','90');
 
 Select * from student Where subject = 'Che' and marks = 90;
+
+
+
+Q9.Write a SQL query to fetch the student details who have scored greater than 80 in Phy or Che.
+
+Create table Student(Studid number,NAME varchar2(10),Subject varchar2(20),marks number);
+insert into student values(1,'A','Phy','90');
+insert into student values(1,'A','Che','95');
+insert into student values(2,'B','Phy','80');
+insert into student values(2,'B','Che','85');
+insert into student values(3,'C','Math','90');
+insert into student values(4,'D','Phy','75');
+insert into student values(4,'D','Math','90');
+
+Select * from Student Where marks > 80 and subject IN ( 'Phy', 'Che');
