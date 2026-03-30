bf181e338c14fde7329124203a929ee57108d414
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 16:16:34 +0400

    Add solution for Q5 (OR operator with ID and Name).

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -80,3 +80,17 @@ Select * from dept Where deptname = 'HR';
 Select * from dept Where deptname IN ('HR', 'SALES');
 
 Select * from dept Where deptname = 'TECH' and Salary = 900;
+
+
+
+-- Q5.Write a SQL query to fetch the employee details whose name is 'A' OR  empid is 2.
+
+Create table employee(empid number,empname varchar2(10),Mgrid number);
+insert into employee values(1,'A',4);
+insert into employee values(2,'B',5);
+insert into employee values(3,'C',6);
+insert into employee values(4,'D',5);
+insert into employee values(5,'E',null);
+insert into employee values(6,'F',null);
+
+Select * from employee Where empname = 'A' OR empid = 2;
