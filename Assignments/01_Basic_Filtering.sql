a9a789158bad084566af90b369c44187842081c7
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 18:07:16 +0400

    Add solution for Q14 (Comparison and BETWEEN operators)

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -241,3 +241,30 @@ insert into employee values(5,'E',5);
 insert into employee values(6,'F',6);
 
 Select * from employee Where mgrid = 5;
+
+
+
+
+-- Q14.Write an SQL query to list the student name 
+a)who has scored marks greater than equal to 80
+b)who has scored less than equal to 90
+c)who has scored marks greater than 50 but less than 95
+d)who has scored marks greater than equal to 50 and less than equal to 95
+
+Create table Student(Studid number,NAME varchar2(10),Subject varchar2(20),marks number);
+insert into student values(1,'A','Phy','90');
+insert into student values(1,'A','Che','95');
+insert into student values(2,'B','Phy','80');
+insert into student values(2,'B','Che','85');
+insert into student values(3,'C','Phy','90');
+insert into student values(4,'D','Phy','75');
+insert into student values(4,'D','Che','90');
+insert into student values(5,'E','Che','95');
+
+Select name from student Where marks > = 80;
+
+Select name from student Where marks < = 90;
+
+Select name from student Where marks > 50 and marks < 95;
+
+Select name from student Where marks between 50 and 95;
