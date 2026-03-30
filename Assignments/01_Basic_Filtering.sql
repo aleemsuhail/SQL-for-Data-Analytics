347c920bb8a9323216ea21575192a5bf69e877de
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 17:15:12 +0400

    Add solution for Q8 (Filtering on Subject and Marks)

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -135,3 +135,19 @@ insert into emp values(9,'H','SALES',300);
 
 Select name from emp Where (Salary > 400 and dept = 'HR') or (Salary < 400 and 
 dept = 'TECH') or (dept = 'SALES');
+
+
+
+
+-- Q8.Write a SQL query to fetch the student details who have scored 90 in Che
+
+Create table Student(Studid number,NAME varchar2(10),Subject varchar2(20),marks number);
+insert into student values(1,'A','Phy','90');
+insert into student values(1,'A','Che','95');
+insert into student values(2,'B','Phy','80');
+insert into student values(2,'B','Che','85');
+insert into student values(3,'C','Phy','90');
+insert into student values(4,'D','Phy','75');
+insert into student values(4,'D','Che','90');
+
+Select * from student Where subject = 'Che' and marks = 90;
