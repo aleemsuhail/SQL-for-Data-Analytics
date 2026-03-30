52134afc82ad966b239125e3a4bcdad7db3b1875
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 17:55:24 +0400

    Add solution for Q12 (Using IN operator for ID list)

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -209,3 +209,19 @@ Select empname from employee Where salary > 150;
 Select empname from employee Where Salary = 100 or empid = 8;
 
 Select empname from employee Where Salary = 100 and empid = 8;
+
+
+
+
+-- Q12. Write a SQL query to fetch the employee details with 
+empid 1 or 2 or 8 using IN operator.
+
+Create table employee(empid number,empname varchar2(10),Mgrid number);
+insert into employee values(1,'A',4);
+insert into employee values(2,'B',5);
+insert into employee values(3,'C',6);
+insert into employee values(4,'D',5);
+insert into employee values(5,'E',null);
+insert into employee values(6,'F',null);
+
+Select * from employee Where empid IN (1, 2, 8);
