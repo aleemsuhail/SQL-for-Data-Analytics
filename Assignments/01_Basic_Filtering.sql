c7ae1e15387f9248da17c087c5bb54a65e963c71
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-30 15:42:21 +0400

    Add solution for Q3 (IS NULL handling)

--- Assignments/01_Basic_Filtering.sql
+++ Assignments/01_Basic_Filtering.sql
@@ -8,10 +8,10 @@ insert into student values(2,'B','Che','85');
 insert into student values(3,'C','Phy','90');
 insert into student values(4,'D','Phy','75');
 insert into student values(4,'D','Che','90');
-Select NAME from Student Where Subject = 'Che' and marks < 90;
 
 B
 
+  Select NAME from Student Where Subject = 'Che' and marks < 90;
 
 -- Q2.Write a SQL query to fetch the student name who has scored 90 or appeared in Che.
 
@@ -31,3 +31,26 @@ C
 D
 
 Select name from Student Where marks = 90 or Subject = 'Che';
+
+
+-- Q3.Write a SQL query to fetch the records where salary is NULL and is from SALES DEPT.
+
+create table dept (dept_id number,deptname varchar2(10),empname varchar2(10),
+salary number);
+
+insert into dept values(1,'HR','A',100);
+insert into dept values(1,'HR','B',200);
+insert into dept values(1,'HR','C',300);
+insert into dept values(1,'HR','X',NULL);
+insert into dept values(2,'SALES','D',400);
+insert into dept values(2,'SALES','E',500);
+insert into dept values(2,'SALES','F',600);
+insert into dept values(2,'SALES','Y',NULL);
+insert into dept values(3,'TECH','G',700);
+insert into dept values(3,'TECH','H',800);
+insert into dept values(3,'TECH','I',900);
+insert into dept values(3,'TECH','Z',NULL);
+
+2, Y, SAELES, NULL.
+
+Select * from dept Where deptname = 'SALES' and Salary is NULL;
