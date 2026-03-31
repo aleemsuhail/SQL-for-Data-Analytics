74c49bf4ff57b075b4f8ce60563edf3cc467e7b4
aleemsuhail <101919293+aleemsuhail@users.noreply.github.com>
2026-03-31 16:34:32 +0400

    Added Dept table with 11 records. Included queries for distinct counts, location-based filtering, and case-insensitive HR department audits.

--- Assignments/02_Aggregations.sql
+++ Assignments/02_Aggregations.sql
@@ -12,3 +12,41 @@ insert into emp values(5,'Kunal','Instructor',400);
 insert into emp values(6,'Yusuf','Instructor',200);
 
 Select Count(*) from emp Where Salary > 500;
+
+-----------------------------------------------------------------------------
+-- Q2.Write an SQL query to
+a)Display the total count of records
+b)Display unique empid
+c)Display unique empname
+d)Display unique DEPT
+e)Display total count of distinct empname
+f)Total count of employess from Delhi
+g)Total count of employees from Delhi working in HR dept.
+-------------------------------------------------------------------------------
+
+Create table Dept(Empid number,Empname varchar2(10),Dept varchar2(10),Location varchar(50));
+insert into dept values(1,'A','HR','Delhi');
+insert into dept values(2,'B','HR','Delhi');
+insert into dept values(3,'C','SALES','Delhi');
+insert into dept values(4,'D','SALES','Chennai');
+insert into dept values(5,'E','SALES','Chennai');
+insert into dept values(6,'F',NULL,'Chennai');
+insert into dept values(7,'G',NULL,'Delhi');
+insert into dept values(1,'A','HR','Chennai');
+insert into dept values(2,'B','HR','Bangalore');
+insert into dept values(3,'C','SALES','Bangalore');
+insert into dept values(8,'j','hr','Delhi');
+
+Select Count(*) from dept;
+
+Select distinct empid from dept;
+
+Select distinct empname from dept;
+
+Select distinct Dept from dept;
+
+Select Count (distinct empname) from dept;
+
+Select Count(*) from dept Where location = 'Delhi';
+
+Select Count(*) from dept Where location = 'Delhi' and UPPER (Dept) = 'HR' ;
